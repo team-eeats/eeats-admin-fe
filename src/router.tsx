ded01a2263b7d5ea829106fe103f06288aecacb8
heedda <heedda.kollim@gmail.com>
2024-10-15 15:16:57 +0900

    ⚙️ :: router 설정

--- src/router.tsx
+++ src/router.tsx
@@ -1,14 +1,18 @@
 import { Route, Routes, BrowserRouter } from "react-router-dom";
-import { GlobalStyle } from "./Styles/GlobalStyle.style";
 import LoginPage from "./Pages/loginPage";
+import { GlobalStyle } from "./Styles/GlobalStyle.style";
+import Layout from "./Components/Layout/index";
 
 function router() {
   return (
     <BrowserRouter>
       <GlobalStyle />
       <Routes>
-        <Route />
+        <Route path="/" element={<Layout />}>
+          <Route path="main" element={<></>} />
+        </Route>
         <Route path="/login" element={<LoginPage />} />
+        <Route path="*" element={<>404 page</>} />
       </Routes>
     </BrowserRouter>
   );
