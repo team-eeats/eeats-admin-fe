0be90511c51f5fd9399bb98501921cdb22171c2f
limda00 <dylim7715@gmail.com>
2024-10-23 20:14:33 +0900

    💄 :: 공지사항 컴포넌트 아이콘 추가

--- src/Components/Notice/index.tsx
+++ src/Components/Notice/index.tsx
@@ -3,22 +3,28 @@ import * as S from "./style";
 import { Font } from "../../Styles/Font";
 import MessageIcon from "../../assets/img/Message.svg";
 import NewMark from "../../assets/img/NewMark.svg";
+import Setting from "../../Assets/img/Setting.svg"
+import ModifyDeleteModal from "../ModifyDeleteModal";
 
 const Notice = () => {
   const [selected, setSelected] = useState(false);
   const [newMessage, setNewMessage] = useState(true);
+  const [modalOpen, setModalOpen] = useState(false)
 
   return (
     <S.Container onClick={() => setSelected(!selected)}>
       <S.AnnouncementWrap>
-        <img src={MessageIcon} alt="메세지" />
-        <S.InfoWrap>
-          <S.TitleWrap>
-            <Font text="오늘 급식 변경" kind="Heading4" />
-            {newMessage ? <img src={NewMark} alt="최신" /> : <></>}
-          </S.TitleWrap>
-          <Font text="1일전" kind="label2" color="gray600" />
-        </S.InfoWrap>
+        <S.IconAndTitleWrap>
+          <img src={MessageIcon} alt="메세지" />
+          <S.InfoWrap>
+            <S.TitleWrap>
+              <Font text="오늘 급식 변경" kind="Heading4" />
+              {newMessage ? <img src={NewMark} alt="최신" /> : <></>}
+            </S.TitleWrap>
+            <Font text="1일전" kind="label2" color="gray600" />
+          </S.InfoWrap>
+        </S.IconAndTitleWrap>
+        <img src={Setting} alt="설정" onClick={() => setModalOpen(!modalOpen)} />
       </S.AnnouncementWrap>
       {selected ? (
         <S.Content>
