f36a458c0ab37f99a779eaca6bf48e71219863d6
heedda <heedda.kollim@gmail.com>
2024-10-24 17:46:13 +0900

    ✨ :: vote Page 퍼블리싱

---
+++ src/Pages/votePage/style.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const VoteContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+  margin-left: 7vw;
+  padding-top: 60px;
+`;
