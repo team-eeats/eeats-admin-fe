d1a05654596b385480a3245d4c6d158f7de6ac45
은지 <wej0513@dsm.hs.kr>
2024-10-28 18:58:42 +0900

    ⚙️ :: comment api 세팅

---
+++ src/Apis/comments/type.ts
@@ -0,0 +1,22 @@
+export type CreateCommentRequest = {
+  content: string;
+};
+
+export type SuggestCommentResponse = {
+  suggestions: Suggestions[];
+};
+
+export type Suggestions = {
+  id: string;
+  userId: string;
+  title: string;
+  content: string;
+  createdAt: string;
+  coment: SuggestComment[];
+};
+
+export type SuggestComment = {
+  id: string;
+  content: string;
+  createdAt: string;
+};
