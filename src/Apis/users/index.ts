de88d8c784c7f145a826a1bae163e1236515ff57
은지 <wej0513@dsm.hs.kr>
2024-10-28 16:53:39 +0900

    🎨 :: 잘못된 코드 삭제

--- src/Apis/users/index.ts
+++ src/Apis/users/index.ts
@@ -1,25 +1,8 @@
 import { instance } from "../axios";
-import {
-  MyPageResponse,
-  MyAllergiesResponse,
-  ModifyMyInfoRequest,
-  SetAllergiesRequest,
-} from "./type";
+import { MyPageResponse } from "./type";
 
 const router = "/users";
 
 export const MyPage = async () => {
   return await instance.get<MyPageResponse>(`${router}/my`);
-};
-
-export const MyAllergies = async () => {
-  return await instance.get<MyAllergiesResponse>(`${router}/allergy`);
-};
-
-export const ModifyMyinfo = async (data: ModifyMyInfoRequest) => {
-  return await instance.patch(`${router}/profile`, data);
-};
-
-export const SetAllergies = async (data: SetAllergiesRequest) => {
-  return await instance.patch(`${router}/allergy`, data);
 };
--- src/Apis/users/type.ts
+++ src/Apis/users/type.ts
@@ -19,28 +19,6 @@ export type Allergies =
   | "SHELLFISH"
   | "PINE_NUTS";
 
-export enum AllergiesNumber {
-  EGGS = 1,
-  DAIRY = 2,
-  BUCKWHEAT = 3,
-  PEANUTS = 4,
-  SOY = 5,
-  WHEAT = 6,
-  MACKEREL = 7,
-  CRAB = 8,
-  SHRIMP = 9,
-  PORK = 10,
-  PEACH = 11,
-  TOMATO = 12,
-  SULPHITES = 13,
-  WALNUTS = 14,
-  CHICKEN = 15,
-  BEEF = 16,
-  SQUID = 17,
-  SHELLFISH = 18,
-  PINE_NUTS = 19,
-}
-
 export type MyPageResponse = {
   accountId: string;
   nickname: string;
@@ -49,21 +27,4 @@ export type MyPageResponse = {
 
 export type MyPageAllergiesType = {
   type: Allergies;
-};
-
-export type MyAllergiesResponse = {
-  allergies: MyAllergy[];
-};
-
-export type MyAllergy = {
-  id: string;
-  type: Allergies;
-};
-
-export type ModifyMyInfoRequest = {
-  nickname: string;
-};
-
-export type SetAllergiesRequest = {
-  type: AllergiesNumber;
 };
