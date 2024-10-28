c048a3f2003292f70446588124fdfb1efa2315c7
limda00 <dylim7715@gmail.com>
2024-10-28 17:16:56 +0900

    💄 :: 페이지 이동 추가

--- src/Pages/Notice/index.tsx
+++ src/Pages/Notice/index.tsx
@@ -2,14 +2,18 @@ import * as S from "./style"
 import { Font } from "../../Styles/Font"
 import Notice from "../../Components/Notice"
 import Write from "../../Assets/img/SVG/Write.svg"
+import { useNavigate } from "react-router-dom"
 
 const NoticePage = () => {
+
+  const navigation = useNavigate()
+
   return (
     <S.Container>
       <S.Content>
         <S.TitleWrap>
           <Font kind="Heading2" text="공지사항" />
-          <S.Button>
+          <S.Button onClick={() => navigation("/addNotice")}>
             <Font kind="Button2" text="공지 작성하기" color="white" />
             <img src={Write} />
           </S.Button>
