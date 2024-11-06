09bb6e9c0c301a0e16c20d50bb6c24943d2bf10e
heedda <heedda.kollim@gmail.com>
2024-11-06 19:50:36 +0900

    ✨ :: instans와 .env파일 수정
    도대체 왜 VITE_를 표기 안 했을까..?

--- src/Apis/axios.ts
+++ src/Apis/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { Cookie } from "../utils/cookie";
 
 export const instance = axios.create({
-  baseURL: process.env.REACT_APP_BASE_URL,
+  baseURL: import.meta.env.VITE_BASE_URL,
   timeout: 3000,
 });
 
