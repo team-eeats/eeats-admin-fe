c38f7ba2dc7e60aa4acf79dad095ce6e2e714821
limda00 <dylim7715@gmail.com>
2024-11-07 22:02:52 +0900

    ✨ :: 공지사항 디테일 API

--- src/Components/Notice/index.tsx
+++ src/Components/Notice/index.tsx
@@ -1,41 +1,62 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import * as S from "./style";
 import { Font } from "../../Styles/Font";
 import MessageIcon from "../../assets/img/Message.svg";
 import NewMark from "../../assets/img/NewMark.svg";
 import Setting from "../../Assets/img/Setting.svg"
 import ModifyDeleteModal from "../ModifyDeleteModal";
+import { NoticeDetailResponse, Notices } from "../../Apis/notices/type";
+import { passOneDay, timeForToday } from "../../utils/time";
+import { NoticeDetail } from "../../Apis/notices";
 
-const Notice = () => {
+const Notice = ({ value }: { value: Notices }) => {
+  const id = value.id
   const [selected, setSelected] = useState(false);
   const [newMessage, setNewMessage] = useState(true);
   const [modalOpen, setModalOpen] = useState(false)
 
+  const [detail, setDetail] = useState<NoticeDetailResponse | null>(null)
+
+  useEffect(() => {
+    const handleDetail = async () => {
+      try {
+        const response = await NoticeDetail(id)
+        setDetail(response.data)
+        setNewMessage(!passOneDay(detail?.createdAt || ''))
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
+    handleDetail()
+  }, [])
+
+
   return (
     <>
-      <S.Container onClick={() => setSelected(!selected)}>
+      <S.Container key={id} onClick={() => setSelected(!selected)}>
         <S.AnnouncementWrap>
           <S.IconAndTitleWrap>
             <img src={MessageIcon} alt="메세지" />
             <S.InfoWrap>
               <S.TitleWrap>
-                <Font text="오늘 급식 변경" kind="Heading4" />
+                <Font text={detail?.title} kind="Heading4" />
                 {newMessage ? <img src={NewMark} alt="최신" /> : <></>}
               </S.TitleWrap>
-              <Font text="1일전" kind="label2" color="gray600" />
+              <Font text={timeForToday(detail?.createdAt || '')} kind="label2" color="gray600" />
             </S.InfoWrap>
           </S.IconAndTitleWrap>
           <img src={Setting} alt="설정" onClick={() => setModalOpen(!modalOpen)} />
         </S.AnnouncementWrap>
         {selected ? (
           <S.Content>
-            <Font text="안녕하세요 블라블라" kind="Body2" />
+            <Font text={detail?.content} kind="Body2" />
           </S.Content>
         ) : (
           <></>
         )}
       </S.Container>
-      { modalOpen && <ModifyDeleteModal />}
+      {modalOpen && <ModifyDeleteModal />}
     </>
   );
 };
