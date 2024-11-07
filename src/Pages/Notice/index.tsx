cdaa1f0c0cce027e92cf42cc1e766b6b3b1503b1
limda00 <dylim7715@gmail.com>
2024-11-07 21:19:03 +0900

    ✨ :: 공지사항 조회 API

--- src/Pages/Notice/index.tsx
+++ src/Pages/Notice/index.tsx
@@ -3,10 +3,27 @@ import { Font } from "../../Styles/Font"
 import Notice from "../../Components/Notice"
 import Write from "../../Assets/img/SVG/Write.svg"
 import { useNavigate } from "react-router-dom"
+import { useEffect, useState } from "react"
+import { NoticeList } from "../../Apis/notices"
+import { NoticeListResponse } from "../../Apis/notices/type"
 
 const NoticePage = () => {
-
   const navigation = useNavigate()
+  const [data, setData] = useState<NoticeListResponse | null>(null)
+
+  useEffect(() => {
+    const handleNotice = async() => {
+      try {
+        const response = await NoticeList()
+        setData(response.data)
+        console.log(response.data)
+      } catch (error) {
+        console.error(error)
+      }
+    }
+
+    handleNotice()
+  }, [])
 
   return (
     <S.Container>
@@ -15,14 +32,13 @@ const NoticePage = () => {
           <Font kind="Heading2" text="공지사항" />
           <S.Button onClick={() => navigation("/addNotice")}>
             <Font kind="Button2" text="공지 작성하기" color="white" />
-            <img src={Write} />
+            <img src={Write} alt="Write icon" />
           </S.Button>
         </S.TitleWrap>
         <S.NoticeWrap>
-          <Notice />
-          <Notice />
-          <Notice />
-          <Notice />
+          {data?.notices.map((value) => (
+            <Notice key={value.id} value={value} />
+          ))}
         </S.NoticeWrap>
       </S.Content>
     </S.Container>
